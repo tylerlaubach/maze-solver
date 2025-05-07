984614707ef19fbaa86f1d7075c40a03b30182ef
Tyler Laubach <tyler.laubach@gmail.com>
2025-05-06 21:30:18 -0400

    add maze randomization

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
     cell_size_y = (screen_y - 2 * margin) / num_rows
     window = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, window)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, window, 0)
 
     window.wait_for_close()
 
--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 import time
+import random
 from cell import Cell
 
 class Maze():
@@ -11,6 +12,7 @@ class Maze():
         cell_size_x,
         cell_size_y,
         window=None,
+        seed=None
     ):
         self._x1 = x1
         self._y1 = y1
@@ -19,10 +21,13 @@ class Maze():
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._window = window
+        if seed:
+            random.seed(seed)
         self._cells = []
 
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0,0)
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -62,3 +67,44 @@ class Maze():
         exit_col, exit_row = len(self._cells) - 1, len(self._cells[0]) - 1
         self._cells[exit_col][exit_row].has_right_wall = False
         self._draw_cell(exit_col, exit_row)
+
+    def _break_walls_r(self, i, j):
+        current_cell = self._cells[i][j]
+        current_cell.visited = True
+        while True:
+            directions = []
+
+            # Check each direction for unvisited neighbors
+            if i > 0 and not self._cells[i - 1][j].visited:
+                directions.append(("left", i - 1, j))
+            if i < self._num_cols - 1 and not self._cells[i + 1][j].visited:
+                directions.append(("right", i + 1, j))
+            if j > 0 and not self._cells[i][j - 1].visited:
+                directions.append(("up", i, j - 1))
+            if j < self._num_rows - 1 and not self._cells[i][j + 1].visited:
+                directions.append(("down", i, j + 1))
+
+            # If no unvisited neighbors, draw and return
+            if not directions:
+                self._draw_cell(i, j)
+                return
+
+            # Choose random direction and get target cell
+            direction, ni, nj = random.choice(directions)
+            next_cell = self._cells[ni][nj]
+
+            # Knock down the walls between current and next cell
+            if direction == "left":
+                current_cell.has_left_wall = False
+                next_cell.has_right_wall = False
+            elif direction == "right":
+                current_cell.has_right_wall = False
+                next_cell.has_left_wall = False
+            elif direction == "up":
+                current_cell.has_top_wall = False
+                next_cell.has_bottom_wall = False
+            elif direction == "down":
+                current_cell.has_bottom_wall = False
+                next_cell.has_top_wall = False
+
+            self._break_walls_r(ni, nj)
